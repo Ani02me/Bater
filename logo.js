443dc665b524ae03a07852e9f61f95ec115c31fc
Elmura <erbakytelmura@gmail.com>
2024-12-04 23:03:01 +0500

    Tapsrma

---
+++ logo.js
@@ -0,0 +1,38 @@
+ //1Tapsrma
+// const students = [
+//   {
+//     name: "Алия",
+//     math: 90,
+//     city: "Алматы",
+//     country: "Қазақстан"
+//   },
+//   {
+//     name: "Бекзат",
+//     math: 85,
+//     city: "Шымкент"
+
+//   }
+// ];
+
+
+// const [firstStudent, secondStudent] = students;
+
+
+// const { name: firstName, math: firstMath, city: firstCity, country: firstCountry = "Қазақстан" } = firstStudent;
+// console.log(`Оқушының аты: ${firstName}, Математика бағасы: ${firstMath}, Қала: ${firstCity}, Ел: ${firstCountry}`);
+
+// const{ name: secondName, math: secondMath, city: secondCity, country: secondCountry = "Қазақстан" } = secondStudent;
+// console.log(`Оқушының аты: ${secondName}, Математика бағасы: ${secondMath}, Қала: ${secondCity}, Ел: ${secondCountry}`);
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// const class1 = ["Айша", "Аслан", "Данияр"];
+// const class2 = ["Мақсат", "Жанар", "Бауыржан"];
+
+// const combinedClass = ["Директор", ...class1, ...class2, "Мұғалім"];
+
+// console.log(combinedClass);
